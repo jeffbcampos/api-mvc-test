4ccbce9457346091c1742cd08f4be208482d7901
Jeferson Campos <jefbcampos@gmail.com>
2023-06-03 12:13:43 -0300

    blue_print module created

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from flask import Flask
 from flask_migrate import Migrate
-from routes.users_routes import users_routes
-from routes.filmes_routes import filmes_routes
-from routes.series_routes import series_routes
+from routes.bluePrints import blue_prints
 from control.MainController import db
 from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity, decode_token
 from flask_cors import CORS
@@ -11,11 +9,10 @@ app = Flask(__name__)
 app.config.from_object('config')
 migrate = Migrate(app, db)
 db.init_app(app)
-app.register_blueprint(users_routes, url_prefix='/')
-app.register_blueprint(filmes_routes, url_prefix='/')
-app.register_blueprint(series_routes, url_prefix='/')
 CORS(app)
 jwt = JWTManager(app)
+for bp in blue_prints:
+    app.register_blueprint(bp)
 
 @app.route('/')
 def main():
---
+++ routes/bluePrints.py
@@ -0,0 +1,5 @@
+from routes.users_routes import users_routes
+from routes.filmes_routes import filmes_routes
+from routes.series_routes import series_routes
+
+blue_prints = [users_routes, filmes_routes, series_routes]
