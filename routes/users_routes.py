e9e50e36d780def3cfebb410f6a425a7c1daf92a
Jeferson Vieira Borges de Campos <jeffbcampos@gmail.com>
2023-07-21 19:58:30 -0300

    atualizando rotas conforme UserController

--- routes/users_routes.py
+++ routes/users_routes.py
@@ -3,8 +3,7 @@ from control.UserController import cadastro, users, checarUsuarios, atualizarUsu
 
 users_routes = Blueprint('users_routes', __name__)
 
-users_routes.route('/users', methods=['GET'])(users)
-users_routes.route('/cadastro', methods=['POST'])(cadastro)
+
 users_routes.route('/usuarios', methods=['POST'])(checarUsuarios)
 users_routes.route('/atualizarUsuario', methods=['POST'])(atualizarUsuario)
 users_routes.route('/atualizarSenha', methods=['POST'])(atualizarSenha)
